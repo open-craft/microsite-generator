b48889f9b5f0bbcd1f397b81f94d4dd2e0f9925f
Shimul Chowdhury <shimul@opencraft.com>
2021-07-26 23:42:09 +0800

    chore: fix typo

--- scripts/const.py
+++ scripts/const.py
@@ -3,5 +3,5 @@ DISCOVERY_ROOT_DIR = '/edx/app/discovery/discovery'
 ECOMMERCE_ROOT_DIR = '/edx/app/ecommerce/ecommerce'
 LMS_ROOT_DIR = '/edx/app/edxapp/edx-platform'
 
-# don't modify this file, as this gets created dynamicly everytime we run the generator
+# don't modify this file, as this gets created dynamically everytime we run the generator
 GENERATED_SHARED_CONFIG_FILE = 'config/_generated.yaml'
